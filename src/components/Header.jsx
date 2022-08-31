19a65ba0621c4ab11ed66e4dd9500f6673c5c863
Anderson-Reges <andersonreges76@gmail.com>
2022-08-31 19:13:20 -0300

    add requisito 04

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 
 class Header extends React.Component {
@@ -21,12 +22,17 @@ class Header extends React.Component {
     const loadingElement = <h2>Carregando...</h2>;
     const { user, loading } = this.state;
     return (
-      loading ? loadingElement
-        : (
-          <header data-testid="header-component">
-            <p data-testid="header-user-name">{ user }</p>
-          </header>
-        )
+      <header data-testid="header-component">
+        <h1>TrybeTunes</h1>
+        <h3
+          data-testid="header-user-name"
+        >
+          { loading ? loadingElement : <p>{ user }</p>}
+        </h3>
+        <Link to="/search"><p data-testid="link-to-search">Search</p></Link>
+        <Link to="/favorites"><p data-testid="link-to-favorites">Favorites</p></Link>
+        <Link to="/profile"><p data-testid="link-to-profile">Profile</p></Link>
+      </header>
     );
   }
 }

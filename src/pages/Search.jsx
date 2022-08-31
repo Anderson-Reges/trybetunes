5d7827e12b4667dad7ba8cd8045414caa44e37ce
Anderson-Reges <andersonreges76@gmail.com>
2022-08-31 19:27:24 -0300

    add requisito 05

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,42 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    disabled: true,
+  };
+
+  handleChange = ({ target }) => {
+    const LENGTH = 2;
+    const { value } = target;
+    if (value.length >= LENGTH) {
+      this.setState({
+        disabled: false,
+      });
+    } else {
+      this.setState({
+        disabled: true,
+      });
+    }
+  };
+
   render() {
+    const { disabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>Search</p>
+        <h3>Search</h3>
+        <input
+          type="text"
+          data-testid="search-artist-input"
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          disabled={ disabled }
+          data-testid="search-artist-button"
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }

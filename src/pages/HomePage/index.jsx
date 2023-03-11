06381815ea1eb0d58a810702700096c4cb282638
Anderson-Reges <andersonreges76@gmail.com>
2023-03-11 02:23:38 -0300

    refac: change padding in home container

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import styles from './styles.module.scss';
@@ -14,7 +15,9 @@ export default class HomePage extends Component {
           <h2>Aqui na DB você tem um banco de dados inteiro de musicas!!!</h2>
           <div className={ styles.buttonBox }>
             <h3>Vem de DB e tenha um gostinho das mais diversas musicas</h3>
-            <input type="button" value="Escute previas agora" />
+            <Link to="/">
+              <input type="button" value="Escute previas agora" />
+            </Link>
           </div>
         </div>
         <div className={ styles.containerBody }>

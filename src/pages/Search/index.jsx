a68a0680cdcfeb81655c849131dd6ba69a3e9e09
Anderson-Reges <andersonreges76@gmail.com>
2023-04-29 13:35:23 -0300

    feat: adding styles in card

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -8,20 +8,11 @@ import styles from './styles.module.scss';
 class Search extends React.Component {
   state = {
     inputSearch: '',
-    prevInputSearch: [],
     disabled: true,
     loading: false,
     hasRender: false,
     requestUrl: '',
-    image: '',
   };
-
-  async componentDidUpdate() {
-    const { image } = await getUser();
-    this.setState({
-      image,
-    });
-  }
 
   handleChange = ({ target }) => {
     const { name, value } = target;
@@ -47,9 +38,9 @@ class Search extends React.Component {
 
   handleSearch = async () => {
     const { inputSearch } = this.state;
-    this.setState((prev) => ({
+    this.setState({
       loading: true,
-      prevInputSearch: [...prev.inputSearch] }));
+    });
     const requestUrl = await searchAlbumsAPI(inputSearch);
     this.setState({
       loading: false,
@@ -61,20 +52,13 @@ class Search extends React.Component {
   };
 
   albumCard = (requestUrl) => {
-    const { prevInputSearch } = this.state;
     if (requestUrl.length === 0) {
       return <p>Nenhum álbum foi encontrado</p>;
     }
     if (requestUrl) {
       return (
         <div>
-          <h4>
-            Resultado de álbuns de:
-            {' '}
-            { prevInputSearch }
-          </h4>
           <div className={ styles.searchContainer }>
-            <input type="button" value="<" />
             <div className={ styles.albumContainer }>
               {requestUrl.map((element) => (
                 <Link
@@ -90,14 +74,13 @@ class Search extends React.Component {
                   <p>{element.artistName}</p>
                 </Link>))}
             </div>
-            <input type="button" value=">" />
           </div>
         </div>);
     }
   };
 
   render() {
-    const { disabled, inputSearch, loading, hasRender, requestUrl, image } = this.state;
+    const { disabled, inputSearch, loading, hasRender, requestUrl } = this.state;
     const albums = (
       <div className={ styles.albums }>
         {this.albumCard(requestUrl)}
@@ -111,22 +94,15 @@ class Search extends React.Component {
             type="text"
             name="inputSearch"
             value={ inputSearch }
-            data-testid="search-artist-input"
             onChange={ this.handleChange }
           />
           <button
             type="button"
             disabled={ disabled }
             onClick={ () => this.handleSearch(inputSearch) }
-            data-testid="search-artist-button"
           >
             Pesquisar
           </button>
-          <Link to="/profile">
-            { image !== ''
-              ? <img src={ image } alt="ImgUser" id={ styles.profilePicture } />
-              : <ion-icon name="person-circle-outline" id={ styles.profileIcon } />}
-          </Link>
         </div>
         {hasRender && albums}
       </div>
